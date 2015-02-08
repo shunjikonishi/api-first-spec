69d43945b8ae881f05a48cf7a0549a9db54f4424
shunjikonishi <shun.konishi@nifty.com>
2015-02-08 22:12:20 +0900

    Implementing

---
+++ README.md
@@ -0,0 +1,9 @@
+# api-first-spec
+A tool for API First development.
+It allows you to describe API as a test.
+
+## Requirement
+- npm
+- mocha
+
+
--- test/login.test.js
+++ test/login.test.js
@@ -1,11 +1,43 @@
 "use strict";
 var 
   assert = require("chai").assert,
-  utils = require("../lib/api-first-spec"),
+  spec = require("../lib/api-first-spec"),
   props = require("./env/properties.json");
 
+spec.defineGlobals();
+
+var API = spec.define({
+  "endpoint": "/api/companies/signin",
+  "method": "POST",
+  "request": {
+    "contentType": ContentType.URLENCODED,
+    "params": {
+      "email": {
+        "type": DataType.STRING,
+        "rules": {
+          "required": true
+        }
+      },
+      "password": {
+        "type": DataType.STRING,
+        "rules": {
+          "required": true
+        }
+      }
+    }
+  },
+  "response": {
+    "contentType": ContentType.JSON,
+    "data": {
+      "code": DataType.STRING,
+      "message": DataType.STRING
+    }
+  }
+});
+console.log(API);
+
 describe("login", function() {
-  var client = utils.createHttpClient(props.host);
+  var client = spec.createHttpClient(props.host);
   it("Wrong username", function(done) {
     client.request({
       "method": "POST",
